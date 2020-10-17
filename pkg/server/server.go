3e9ba41b70d8c7f8d0eb70ec357677f65f76e65d
rodriguezst <carlos@rodriguezst.es>
2020-10-17 18:15:13 +0200

    Implemented regex-filters for channel names and groups
    See docker-compose for usage instructions.
    - Added regex filtering to choose only some groups of the M3U playlist (Example: only SPORTS and NEWS groups)
    - Added regex filtering to choose only some channels (Example: only those including HD in the name)

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,3 +1,4 @@
+
 /*
  * Iptv-Proxy is a project to proxyfie an m3u file and to proxyfie an Xtream iptv service (client API).
  * Copyright (C) 2020  Pierre-Emmanuel Jacquier
@@ -23,6 +24,7 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"regexp"
 
 	"github.com/jamesnetherton/m3u"
 	"github.com/pierre-emmanuelJ/iptv-proxy/pkg/config"
@@ -97,17 +99,27 @@ func (c *Config) playlistInitialization() error {
 func (c *Config) marshallInto(into *os.File, xtream bool) error {
 	into.WriteString("#EXTM3U\n") // nolint: errcheck
 
+	re_group := regexp.MustCompile(c.GroupRegex)
+	re_channel := regexp.MustCompile(c.ChannelRegex)
+
 TRACKS_LOOP:
 	for _, track := range c.playlist.Tracks {
 
-		// Groups filtering
-		if len(c.FilterGroups) > 0 {
+		// Group regex
+		if c.GroupRegex != "" {
 			for i := range track.Tags {
 				name := track.Tags[i].Name
 				value := track.Tags[i].Value
-				if name == "group-title" && !c.FilterGroups[value] {
+				if name == "group-title" && !re_group.MatchString(value) {
 					continue TRACKS_LOOP
 				}
+			}
+		}
+
+		// Channel regex
+		if c.ChannelRegex != "" {
+			if !re_channel.MatchString(track.Name) {
+				continue TRACKS_LOOP
 			}
 		}
 
